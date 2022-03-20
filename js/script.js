528a2f4eae966f379623733390f641def1ad8981
Jarek <dojarka0@gmail.com>
2022-03-20 17:43:24 +0100

    finishing menu

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@ const menu = document.querySelector(".menu");
 const item = document.querySelectorAll(".dropdown");
 const itemText = document.querySelectorAll(".item-text");
 
+let double = 0;
+let text = "";
+
 for (let i = 0; i < itemText.length; i++) {
   itemText[i].addEventListener("click", () => {
+    double++;
+
     item.forEach((el) => {
       el.classList.remove("active");
     });
-
     item[i].classList.toggle("active");
+
+    if (itemText[i].textContent == text && double == 2) {
+      item[i].classList.remove("active");
+      double = 0;
+    }
+
+    if (double == 2) double = 0;
+    text = itemText[i].textContent;
   });
 }
 
@@ -20,3 +32,13 @@ hamburger.addEventListener("click", () => {
   });
   menu.classList.toggle("active");
 });
+
+// for (let i = 0; i < itemText.length; i++) {
+//   itemText[i].addEventListener("click", () => {
+//     item.forEach((el) => {
+//       el.classList.remove("active");
+//     });
+
+//     item[i].classList.toggle("active");
+//   });
+// }
